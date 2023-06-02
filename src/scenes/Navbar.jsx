51a6049e9a1ab0147932486ee64df56da377fd99
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-06-02 21:40:06 +0530

    update

---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+const Navbar = ({ selectedPage, setSelectedPage }) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-poppins text-3xl font-bold">AT</h4>
+
+        {/* Desktop Nav*/}
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold"></div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </nav>
+  );
+};
