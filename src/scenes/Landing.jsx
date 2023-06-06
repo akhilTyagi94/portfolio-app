f2dc580cc6155bac5bb90bc0e289ab3d3e967408
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-06-07 00:36:26 +0530

    update

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -21,8 +21,40 @@ const Landing = ({ setSelectedPage }) => {
             />
           </div>
         ) : (
-          <div></div>
+          <img
+            alt="profile"
+            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+            src="assets/profile-image.png"
+          />
         )}
+      </div>
+
+      {/** Main Section */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* Headings */}
+        <motion.div
+          intial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-poppins z-10 text-center md:text-start">
+            Akhil {""}
+            <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush before:absolute before:-left-[30px] before:-top-[70px] before:z-[-1]">
+              Tyagi
+            </span>
+          </p>
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </motion.div>
       </div>
     </section>
   );
