d366b9e5f0c8272e0e1f4d3749b44e22fd89428a
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-06-13 00:13:03 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import LineGradient from "./components/LineGradient";
 import MySkills from "./scenes/MySkills";
+import Projects from "./scenes/Projects";
+import Testimonials from "./scenes/Testimonials";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -41,6 +45,19 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
         <MySkills />
       </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Projects />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Testimonials />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
